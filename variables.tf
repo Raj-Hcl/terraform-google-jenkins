9355b80304a3aaf609a3822c4bb12d1723c066fd
Raj-Hcl <raj.tripathi@hcltech.com>
2024-10-14 20:32:25 +0530

    Update variables.tf

--- variables.tf
+++ variables.tf
@@ -1,49 +1,50 @@
-variable GOOGLE_CREDENTIALS{
+variable GOOGLE_CREDENTIALS {
     type = string
 }
-variable myname{
+variable myname {
     type = string
     description = "Name for vm instance"
 }
-variable mymachine{
+variable mymachine {
     type = string
     description = "Value for machine-type"
 }
-variable myzone{
+variable myzone {
     type = string
     description = "Value for zone"
 }
-variable image{
+variable image {
     type = string
     description = "Value for boot disk image"
 }
-variable firename{
+variable firename {
     type = string
     description = "Name for firewall"
 }
-variable fireIP{
+variable fireIP {
     type = list(string)
     description = "Value for source_ranges in firewall"
 }
-variable vpcname{
+variable vpcname {
     type = string
     description = "Name for vpc"
 }
-variable subname{
+variable subname {
     type = string
     description = "Name for subnet"
 }
-variable subrange{
+variable subrange {
     type = string
     description = "Value for subnet IP range"
 }
-variable region{
+variable region {
     type = string
     description = "Value for region"
 }
-variable port{
+variable port {
     type = list(string)
     description = "Value for firewall port"
+    default = []
 }
 variable protocol{
     type=string
